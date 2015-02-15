092b32b798f75ee812f3349b472e213bedb9c872
Behzad Shirani <behzad.shirani@yahoo.com>
2015-02-15 10:11:02 +0330

    FirstCommit

---
+++ EpedcRequestCompare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EpedcRequestCompare.MvcApplication" Language="C#" %>
---
+++ EpedcRequestCompare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EpedcRequestCompare.Startup))]
+namespace EpedcRequestCompare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
